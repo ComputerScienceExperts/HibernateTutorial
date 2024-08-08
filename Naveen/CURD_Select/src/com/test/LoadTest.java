018e5d05b9bad588c95131a04efa2122ca8e034e
Chandan Roy <123297138+ComputerScienceExperts@users.noreply.github.com>
2024-08-08 16:57:20 +0530

    Add files via upload

---
+++ Naveen/CURD_Select/src/com/test/LoadTest.java
@@ -0,0 +1,32 @@
+package com.test;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.beans.Student;
+
+public class LoadTest {
+
+	public static void main(String[] args) {
+		Configuration cfg = new Configuration();
+		cfg.configure("resource/mysql.cfg.xml");
+		SessionFactory sf= cfg.buildSessionFactory();
+		Session session = sf.openSession();
+		
+		
+		Object o =session.load(Student.class, 112);
+		Student s=  (Student)o;
+		System.out.println(s.getSid());
+		System.out.println(s.getSname());
+		System.out.println(s.getSemail());
+		System.out.println(s.getSfee());
+		
+		session.close();
+		sf.close();
+		
+		
+
+	}
+
+}
