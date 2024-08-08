6383dc90577d375c55cf642d8114ee1664c2e78d
Chandan Roy <123297138+ComputerScienceExperts@users.noreply.github.com>
2024-08-08 17:13:34 +0530

    Add files via upload

---
+++ Naveen/HibernateCurdApplication/src/com/beans/Account.java
@@ -0,0 +1,41 @@
+package com.beans;
+
+import java.sql.Date;
+
+public class Account {
+	private long accno;
+	private String name;
+	private double balance;
+
+	public long getAccno() {
+		return accno;
+	}
+
+	public void setAccno(long accno) {
+		this.accno = accno;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	@Override
+	public String toString() {
+		return "Account [accno=" + accno + ", name=" + name + ", balance=" + balance + "]";
+	}
+
+	
+
+}
---
+++ Naveen/HibernateCurdApplication/src/com/service/AccountService.java
@@ -0,0 +1,39 @@
+package com.service;
+
+import com.beans.Account;
+import com.dao.AccountDao;
+
+public class AccountService {
+	public static void main(String[] args) {
+
+		AccountDao dao = new AccountDao();
+
+		/*
+		 * // Retrieve Account Account rAccount = dao.get(90005);
+		 * System.out.println("Account details ...."); System.out.println("Accno : " +
+		 * rAccount.getAccno()); System.out.println("Name : " + rAccount.getName());
+		 * System.out.println("8alance : " + rAccount.getBalance());
+		 */
+
+		// Create Account
+		Account cAccount = new Account();
+		cAccount.setAccno(90005);
+		cAccount.setName("sekhar");
+		cAccount.setBalance(6899);
+		dao.insert(cAccount);
+		System.out.println("Account created successfully");
+
+		/*
+		 * // Update Account Account uAccount = new Account(); uAccount.setAccno(90005);
+		 * uAccount.setName("sekhareddy"); uAccount.setBalance(4500);
+		 * dao.update(uAccount); System.out.println("Account updated successfully");
+		 */
+
+		/*
+		 * // Delete Account dao.delete(90005);
+		 * System.out.println("Account is deleted successfully");
+		 */
+
+	}
+
+}
